2eabc7aad5c90e28277dff2b9a7c9438c58c4b2e
rohan <rohanmalo@gmail.com>
2024-01-15 19:41:08 +0530

    store created and wrap with provider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./counter.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className='parent'>
+        <button className='btn'>+</button>
+        <p className='count'>0</p>
+        <button className='btn'>-</button>
+      </div>
     </div>
   );
 }
