2f63956f5dd46741057804d3c0e553ca8f3484b8
rohan <rohanmalo@gmail.com>
2024-01-16 22:02:49 +0530

    fetch and update the state using useSelect and useDispatch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./counter.css"
+import { useSelector, useDispatch } from 'react-redux';
+import {increment, decrement} from "./state/counterSclice"
 
 function App() {
+
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  const handleincrement = ()=> {
+    dispatch(increment())
+  }
+
+  const handledecrement = () => {
+    dispatch(decrement())
+  }
+  
   return (
     <div className="">
       <div className='parent'>
-        <button className='btn'>+</button>
-        <p className='count'>0</p>
-        <button className='btn'>-</button>
+        <button className='btn' onClick={handleincrement}>+</button>
+        <p className='count'>{count}</p>
+        <button className='btn' onClick={handledecrement}>-</button>
       </div>
     </div>
   );
